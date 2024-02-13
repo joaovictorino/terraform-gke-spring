a504be8b4b249053a5f3cfd48f638915c2893969
joaovictorino <jhvictorino@gmail.com>
2024-02-12 23:05:10 -0300

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,21 @@ docker build -t springapp .
 docker tag springapp:latest us-east1-docker.pkg.dev/teste-sample-388301/ar-aula-spring/springapp:latest
 
 # login no repositorio de imagem do Artifact Registry (privado)
+<<<<<<< HEAD
 gcloud auth configure-docker us-east1-docker.pkg.dev
+=======
+gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
+>>>>>>> 3db0556 (initial commit)
 
 # subir imagem
 docker push us-east1-docker.pkg.dev/teste-sample-388301/ar-aula-spring/springapp:latest
 
 # obter credenciais do GKE
+<<<<<<< HEAD
 gcloud container clusters get-credentials  gke-aula-infra --region us-east1
+=======
+gcloud container clusters get-credentials gke-aula-infra --region us-central1 --quiet
+>>>>>>> 3db0556 (initial commit)
 
 # subir configuração da aplicação
 kubectl apply -f gke/1-config
